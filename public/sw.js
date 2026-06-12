c7e02cd54e3f04c7cdef329813f64e9f7e65fa75
jean-francoisdumetz-ux <jean-francois.dumetz@fidal.notaires.fr>
2026-06-12 23:42:30 +0200

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,4 @@
+// Service worker minimal : rend l'application installable.
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", (e) => e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch", () => {});
